1eedfae090a7f0657079f568656f708e91e44f60
Lycert-idat <lycert1@gmail.com>
2026-05-29 22:13:58 -0500

    Semana3-Primer Commit

---
+++ app/src/main/java/com/example/idatdemo/ListaComprasActivity.kt
@@ -0,0 +1,60 @@
+package com.example.idatdemo
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+class ListaComprasActivity : AppCompatActivity() {
+
+    private lateinit var lvproductos : ListView
+    private lateinit var txtbuscar : TextInputEditText
+    private lateinit var btninsert : MaterialButton
+
+    private val productos = mutableListOf<String>()
+    private lateinit var productosadapter : ArrayAdapter<String>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lista_compras)
+
+        lvproductos = findViewById(R.id.lvproductos)
+        txtbuscar = findViewById(R.id.txtbuscar)
+        btninsert = findViewById(R.id.btninsert)
+        productos.add("Arroz")
+        productos.add("Azucar")
+        productosadapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            productos
+        )
+
+        lvproductos.adapter = productosadapter
+        btninsert.setOnClickListener{
+            val producto = txtbuscar.text.toString().trim()
+            if(producto.isNotEmpty()){
+                productos.add(producto)
+                productosadapter.notifyDataSetChanged()
+                txtbuscar.text?.clear()
+            }
+            else{
+                Toast.makeText(this,"Ingrese el valor",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/idatdemo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.idatdemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvRegistro : TextView
+    private lateinit var btnentrar : MaterialButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        tvRegistro = findViewById(R.id.tvRegistro)
+        tvRegistro.setOnClickListener {
+            var intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+        btnentrar = findViewById(R.id.btnentrar)
+        btnentrar.setOnClickListener{
+            val intent = Intent(this,ListaComprasActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
