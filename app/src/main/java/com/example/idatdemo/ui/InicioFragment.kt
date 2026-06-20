66aac8f7bd69a962e14d0eac7041c23222d6f9e2
felyp244 <felipe.yan12@gmail.com>
2026-06-19 21:47:17 -0500

    Fragments en blanco añadidos

---
+++ app/src/main/java/com/example/idatdemo/ui/InicioFragment.kt
@@ -0,0 +1,11 @@
+package com.example.idatdemo.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.idatdemo.R
+
+
+class InicioFragment : Fragment(R.layout.fragment_inicio)
---
+++ app/src/main/java/com/example/idatdemo/ui/PerfilFragment.kt
@@ -0,0 +1,10 @@
+package com.example.idatdemo.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.idatdemo.R
+
+class PerfilFragment : Fragment(R.layout.fragment_perfil)
