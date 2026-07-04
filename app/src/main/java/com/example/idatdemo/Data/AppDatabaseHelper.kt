f3cf3e18a6374f839d19ece080bd76a4a2f0e57c
Lycert-idat <lycert1@gmail.com>
2026-07-03 21:36:13 -0500

    Añadido sqlite: La tabla productos y se diseño el formulario para insertar a la tabla

---
+++ app/src/main/java/com/example/idatdemo/Data/AppDatabaseHelper.kt
@@ -0,0 +1,27 @@
+package com.example.idatdemo.Data
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class AppDatabaseHelper(context : Context) : SQLiteOpenHelper(context, "productos.db",null,2){
+    override fun onCreate(p0: SQLiteDatabase) {
+        p0.execSQL("""
+            create table producto(
+                id INTEGER Primary Key Autoincrement not null,
+                title Text,
+                Price REAL,
+                description TEXT,
+                category TEXT,
+                image TEXT
+                );
+""".trimIndent())
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase, p1: Int, p2: Int) {
+        p0.execSQL("Drop table if exists producto")
+        onCreate(p0)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/idatdemo/repository/ProductoRepository.kt
@@ -0,0 +1,23 @@
+package com.example.idatdemo.repository
+
+import android.content.ContentValues
+import android.content.Context
+import com.example.idatdemo.Data.AppDatabaseHelper
+import com.example.idatdemo.entity.Producto
+
+class ProductoRepository(context: Context) {
+
+    private val dbhelper = AppDatabaseHelper(context)
+
+    fun insertar(producto : Producto): Long{
+        val db = dbhelper.writableDatabase
+        val valores = ContentValues().apply {
+            put("id", producto.id)
+            put("title",producto.title)
+            put("price",producto.price)
+            put("description",producto.description)
+            put("image",producto.image)
+        }
+        return db.insert("producto",null,valores)
+    }
+}
