1be2ddf425c89dcf0b2dc2d4cebdc8fdca3983e6
felypprueba <felipe.yan12@gmail.com>
2026-07-31 21:31:34 -0500

    Firebase implementado con una base de datos

--- app/src/main/java/com/example/idatdemo/MainActivity.kt
+++ app/src/main/java/com/example/idatdemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var tvRegistro : TextView
     private lateinit var btnentrar : MaterialButton
-
+    private lateinit var tvtitulo : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,6 +29,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, InicioActivity::class.java)
             startActivity(intent)
         }
+        //Otra forma de cambiar el tipo de letra
+//        tvtitulo = findViewById(R.id.tvtitulo)
+//        val cambiarletra = ResourcesCompat.getFont(this,R.font.quicksandregular)
+//        tvtitulo.typeface= cambiarletra
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/idatdemo/NuevoProductoActivity.kt
+++ app/src/main/java/com/example/idatdemo/NuevoProductoActivity.kt
@@ -1,6 +1,7 @@
 package com.example.idatdemo
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -11,6 +12,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.idatdemo.entity.Producto
 import com.example.idatdemo.entity.Rating
 import com.example.idatdemo.repository.ProductoRepository
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+import java.util.UUID
 
 class NuevoProductoActivity : AppCompatActivity() {
     private lateinit var ettitle : EditText
@@ -39,23 +43,42 @@ class NuevoProductoActivity : AppCompatActivity() {
             val description = etdescription.text.toString()
             val category = etcategory.text.toString()
             val imagen = etimage.text.toString()
-            val productoRepository = ProductoRepository(this)
-            val idproducto =productoRepository.insertar(
-                Producto(
-                    id=0,
-                    title = title,
-                    price = price,
-                    category = category,
-                    description = description,
-                    image = imagen,
-                    rating = Rating(
-                        rate=0.0,
-                        count = 0
-                    )
-                )
+            val db = Firebase.database.reference
+            val productomap = mapOf(
+                "title" to title,
+                "price" to price,
+                "description" to description,
+                "category" to category,
+                "image" to imagen
             )
-            Toast.makeText(this,"Producto insertado con id: $idproducto",Toast.LENGTH_SHORT).show()
-            finish()
+            val uuid = UUID.randomUUID().toString()
+            db.child("productos").child(uuid).setValue(productomap)
+                .addOnSuccessListener {
+                    Log.e("Firebase","Producto Insertado")
+                    Toast.makeText(this,"Producto insertado",Toast.LENGTH_SHORT).show()
+                    finish()
+
+                }
+                .addOnFailureListener{error->
+                    Log.e("Firebase",error.toString())
+                    Toast.makeText(this,"Producto no insertado",Toast.LENGTH_SHORT).show()
+                }
+//            val productoRepository = ProductoRepository(this)
+//            val idproducto =productoRepository.insertar(
+//                Producto(
+//                    id=0,
+//                    title = title,
+//                    price = price,
+//                    category = category,
+//                    description = description,
+//                    image = imagen,
+//                    rating = Rating(
+//                        rate=0.0,
+//                        count = 0
+//                    )
+//                )
+//            )
+
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
